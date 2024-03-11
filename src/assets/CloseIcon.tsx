596dd8a3ef50eb3d3d2263e1bd7f0afce4483dca
mehmetasilkilic <mehmetasil96@gmail.com>
2024-03-11 19:02:39 +0300

    navbar

---
+++ src/assets/CloseIcon.tsx
@@ -0,0 +1,18 @@
+const CloseIcon = () => (
+  <svg
+    className="h-6 w-6 animate-x-to-hamburger"
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M6 18L18 6M6 6l12 12"
+    ></path>
+  </svg>
+);
+
+export default CloseIcon;
